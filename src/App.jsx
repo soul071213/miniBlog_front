e8a29a0728ecde251ca9698b8b1269a0a835be59
soul071213 <js071213asd@gmail.com>
2025-02-24 22:04:44 +0900

    회원가입 로그인 퍼블리싱 완료

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/main/page';
 import DetailPage from './pages/detail/page';
+import LoginPage from './pages/login/page';
+import SignUp from './pages/signUp/page';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path='/login' element={<LoginPage/>}></Route>
+        <Route path='/singup' element={<SignUp/>}></Route>
         <Route path="/detail/:id" element={<DetailPage />} />
+        
       </Routes>
     </BrowserRouter>
   );
--- src/pages/signUp/page.jsx
+++ src/pages/signUp/page.jsx
@@ -1,3 +1,60 @@
+import styled from "styled-components";
+import Title from "../../components/title";
+
+const Wrapper = styled.div`
+    width: 100%;
+    padding: 0 10%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const GrayContainer = styled.div`
+    width: 100%;
+    height: 360px;
+    background-color: lightgrey;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    padding: 48px 10%;
+`;
+
+const InputContainer = styled.input`
+    width: 100%;
+    height: 77px;
+    background-color: white;
+    border-radius: 8px;
+    border: none;
+    font-size: 24px;
+    padding: 12px;
+`;
+
+const SubmitButton = styled.button`
+    width: 275px;
+    height: 64px;
+    display: flex;
+    align-items: center;
+    border: none;
+    justify-content: center;
+    font-size: 24px;
+    border-radius: 12px;
+    background-color: white;
+`;
+
 export default function SignUp(){
-    
+    return(
+        <> 
+            <Wrapper>
+                <Title></Title>
+                <GrayContainer>
+                    <InputContainer></InputContainer>
+                    <InputContainer></InputContainer>
+                    <SubmitButton>회원가입</SubmitButton>
+                </GrayContainer>
+            </Wrapper>
+        </>
+    );
+
 }
