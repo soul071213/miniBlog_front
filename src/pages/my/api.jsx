194138eec9c9b37f7aad31301cd4c828f093f028
soul071213 <js071213asd@gmail.com>
2025-03-14 10:30:28 +0900

    log api 연결

--- src/pages/my/api.jsx
+++ src/pages/my/api.jsx
@@ -22,3 +22,15 @@ const logout = async (AccessToken)=>{
         return [];
     }
 }
+
+const log = async (AccessToken)=>{
+    try{
+        const res = await axios.post(`${API_BASE_URL}/log`,{headers:{Authorization:AccessToken}});
+        return res.status;
+    }
+    catch(err){
+        console.error(err);
+        return [];
+    }
+}
+
